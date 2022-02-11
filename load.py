65390edb59aee3b28523e9751db02decc29798d3
hugo <hugo.mathh@gmail.com>
2022-02-11 15:10:31 +0100

    Added ETL connection to postgres

--- load.py
+++ load.py
@@ -3,17 +3,26 @@ from sys import displayhook
 import pandas as pd
 import os, glob
 import kaggle
+import psycopg2
+from configparser import ConfigParser
+
+
+USER="root"
+PASSWORD="informatiquedecisionnelle"
 
 dataset_path = 'dataset'
 
 class ETL:
-    def __init__(self) -> None:
+    def __init__(self, user_, password_, db_, server_="localhost", port_=5432) -> None:
         self.current_path = os.getcwd()
+        
+        print("[*] Connecting to %s .."%(db_))
+        self.conn, self.cur = ETL.connect()
+        print("[*] Done !")
+
 
     def extract(self) -> None:
-        """ 
-        extract dataset from kaggle account
-        """
+        """ extract dataset from kaggle account and csv files """
         self.csv_files = glob.glob(os.path.join(dataset_path, "*.csv"))
         # getting data from kaggle 
         if not self.csv_files:
@@ -36,11 +45,70 @@ class ETL:
             print('Content:')
             displayhook(df)
             print()
+    
+    def transform(self):
+        """ Transform data to our needs .. """
+        pass
+
+    def load(self):
+        """ Load current csv to GCP BDD """
+        pass
+
+    @staticmethod
+    def connect():
+        """ connect to postgres server"""
+        conn = None
+        cur = None
+        try:
+            # read connection parameters
+            params = ETL.config()
+
+            # connect to the PostgreSQL server
+            print('Connecting to the PostgreSQL database...')
+            conn = psycopg2.connect(**params)
+		
+            # create a cursor
+            cur = conn.cursor()
+        
+            # execute a statement
+            print('PostgreSQL database version:')
+            cur.execute('SELECT version()')
+
+            # display the PostgreSQL database server version
+            db_version = cur.fetchone()
+            print(db_version)
+        
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            return conn, cur
+
+    @staticmethod
+    def config(filename='database.ini', section='postgresql'):
+        """ get config file for the postgres connection """
+        # create a parser
+        parser = ConfigParser()
+        # read config file
+        parser.read(filename)
+
+        # get section, default to postgresql
+        db = {}
+        if parser.has_section(section):
+            params = parser.items(section)
+            for param in params:
+                db[param[0]] = param[1]
+        else:
+            raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+        return db
+        
 
 #pandas.DataFrame.to_sql. -> apres on le stock en bdd et requete
 # 1er couche 
 # 2eme nettoyage, selection,
 # 3eme couche, presenter les données
 
-etl = ETL()
-etl.extract()
+etl = ETL(USER, PASSWORD, 'f1-1950-2021')
+"""etl.extract()
+etl.transform()
+etl.load()"""
